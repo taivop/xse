878f74c4f89f9362f5a24535747e006bef8bdaf2
AnnabellKuldmaa <annabell.kuldmaa@gmail.com>
2014-10-09 13:39:11 +0300

    modified IntroUI

--- POS/src/ee/ut/math/tvt/TRAtarkvaratehnika/Intro.java
+++ POS/src/ee/ut/math/tvt/TRAtarkvaratehnika/Intro.java
@@ -1,7 +1,18 @@
 package ee.ut.math.tvt.TRAtarkvaratehnika;
 
-public class Intro {
+import java.io.IOException;
+//import org.apache.log4j.Logger;
 
-	// TODO: launch IntroUI
-	
+public class Intro {
+	//private static final Logger log = Logger.getLogger(Intro.class);
+	public static void main(String[] args) {
+		// Initialise the logger
+	//	Logger log = Logger.getLogger(Intro.class);
+		try {
+			IntroUI.Window();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//log.info("UI Window started!");
+	}
 }
--- POS/src/ee/ut/math/tvt/TRAtarkvaratehnika/IntroUI.java
+++ POS/src/ee/ut/math/tvt/TRAtarkvaratehnika/IntroUI.java
@@ -1,16 +1,80 @@
 package ee.ut.math.tvt.TRAtarkvaratehnika;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
 
-public class IntroUI {
-	
-	// TODO:
-	/* When class is intialized, it should open a window
-	 * where must be the following information:
-	 *	Team name
-		Team leader
-		Team leader email
-		Team members
-		Team logo (random image)
-		Your software version number
-	 */
-
+class Window extends JFrame
+{
+	Window() throws IOException
+	{
+		setTitle("POS system"); 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500, 400);
+		//from application.properties
+		Properties appProp = new Properties();
+		FileInputStream appIn = new FileInputStream("application.properties");
+		appProp.load(appIn);
+		appIn.close();
+		//from version.properties
+		Properties verProp = new Properties();
+		FileInputStream verIn = new FileInputStream("version.properties");
+		verProp.load(verIn);
+		verIn.close();
+		setLayout(new GridBagLayout());
+		String[] data = {
+				"Team Name",
+				appProp.getProperty("team_name").replaceAll("\"", ""),
+		        "Team Leader",
+		        appProp.getProperty("team_leader").replaceAll("\"", ""),
+		        "Team Leader email",
+		        appProp.getProperty("team_leader_email").replaceAll("\"", ""),
+		        "Team members",
+		        appProp.getProperty("team_members").replaceAll("\"", ""),
+		        "Build number",
+		        verProp.getProperty("build.number").replaceAll("\"", "")
+		        };
+		//Gridi asjade paigutamine.
+		GridBagConstraints c = new GridBagConstraints();
+	JLabel silt = (new JLabel());
+		Integer luger = 0;
+		for (int i=0;i<5;i++){
+			c.gridy = i;
+			for (int j=0;j<2;j++){
+				c.gridx = j;
+				if (j==0){
+					c.weightx = 0.3;
+				}
+				else{
+					c.weightx = 0.7;
+				}
+				silt = new JLabel(data[luger]);
+				add(silt,c);
+				luger++;		
+			}
+		}
+	/*	//Pildi lisamine
+		GridBagConstraints d = new GridBagConstraints();
+		ImageIcon icon = new ImageIcon("team_image.jpg");
+		silt = new JLabel(icon);
+		d.fill = GridBagConstraints.HORIZONTAL;
+		d.gridy = 5;
+		d.gridwidth = 2;
+		d.anchor = GridBagConstraints.CENTER;
+		add(silt,d);*/
+		setVisible(true);
+	}
+}
+public class IntroUI
+{
+		public static void Window() throws IOException 
+		{
+			Window frame = new Window();			
+		}
 }
