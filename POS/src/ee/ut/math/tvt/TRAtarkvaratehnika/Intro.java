94e35e91df91a07f1a7af51f3a76abfbb023f187
taivop <tpungas@ut.ee>
2014-10-11 13:28:55 +0300

    log4j works (added configuration)

--- POS/src/ee/ut/math/tvt/TRAtarkvaratehnika/Intro.java
+++ POS/src/ee/ut/math/tvt/TRAtarkvaratehnika/Intro.java
@@ -1,18 +1,24 @@
 package ee.ut.math.tvt.TRAtarkvaratehnika;
 
 import java.io.IOException;
-//import org.apache.log4j.Logger;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class Intro {
-	//private static final Logger log = Logger.getLogger(Intro.class);
+	private static final Logger log = Logger.getLogger(Intro.class);
+	
 	public static void main(String[] args) {
 		// Initialise the logger
-	//	Logger log = Logger.getLogger(Intro.class);
+		Logger log = Logger.getLogger(Intro.class);
+		BasicConfigurator.configure();
+		
 		try {
 			IntroUI.Window();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//log.info("UI Window started!");
+		
+		log.info("UI Window started!");
 	}
 }
