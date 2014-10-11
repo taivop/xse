6d06adff69c2675b437a6e085c5fb9251627bef3
FrauBeka <FrauBeka@Rebeka>
2014-10-11 13:29:21 +0300

    Team picture

--- POS/src/ee/ut/math/tvt/TRAtarkvaratehnika/Intro.java
+++ POS/src/ee/ut/math/tvt/TRAtarkvaratehnika/Intro.java
@@ -1,18 +1,18 @@
 package ee.ut.math.tvt.TRAtarkvaratehnika;
 
 import java.io.IOException;
-//import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;
 
 public class Intro {
 	//private static final Logger log = Logger.getLogger(Intro.class);
 	public static void main(String[] args) {
 		// Initialise the logger
-	//	Logger log = Logger.getLogger(Intro.class);
+		Logger log = Logger.getLogger(Intro.class);
 		try {
 			IntroUI.Window();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//log.info("UI Window started!");
+		log.info("UI Window started!");
 	}
 }
