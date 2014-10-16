7bf1ed57deaf3863112f9b1f94bf10c094849085
taivop <tpungas@ut.ee>
2014-10-16 12:33:41 +0300

    base code to Intro.java

--- POS/src/ee/ut/math/tvt/TRAtarkvaratehnika/Intro.java
+++ POS/src/ee/ut/math/tvt/TRAtarkvaratehnika/Intro.java
@@ -1,24 +1,54 @@
 package ee.ut.math.tvt.TRAtarkvaratehnika;
 
-import java.io.IOException;
-
 import org.apache.log4j.Logger;
-import org.apache.log4j.xml.DOMConfigurator;
+
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 
 public class Intro {
+
 	private static final Logger log = Logger.getLogger(Intro.class);
+	private static final String MODE = "console";
 
 	public static void main(String[] args) {
-		// Init. logger
-		Logger log = Logger.getLogger(Intro.class);
-		DOMConfigurator.configure("etc/log4j.xml");
-		//BasicConfigurator.configure();
-		
-		try {
-			IntroUI.Window();
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			IntroUI introUI = new IntroUI();
+			introUI.setVisible(true);
+			introUI.setAlwaysOnTop(true);
+
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+
+			introUI.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			introUI.setVisible(false);
 		}
-		log.info("Intro window opened");
 	}
 }
+
+/*
+ * public class Intro { private static final Logger log =
+ * Logger.getLogger(Intro.class);
+ * 
+ * public static void main(String[] args) { // Init. logger Logger log =
+ * Logger.getLogger(Intro.class); DOMConfigurator.configure("etc/log4j.xml");
+ * //BasicConfigurator.configure();
+ * 
+ * try { IntroUI.Window(); } catch (IOException e) { e.printStackTrace(); }
+ * log.info("Intro window opened"); } }
+ */
