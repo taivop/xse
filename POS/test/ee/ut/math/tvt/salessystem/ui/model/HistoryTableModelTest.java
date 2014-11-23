bd6e579da20c4ac56525ae71d3fae08a47166d31
Rebeka Mändar <rebeka.m2ndar@gmail.com>
2014-11-23 20:59:21 +0200

    HistoryTableModelTest updated #50

--- POS/test/ee/ut/math/tvt/salessystem/ui/model/HistoryTableModelTest.java
+++ POS/test/ee/ut/math/tvt/salessystem/ui/model/HistoryTableModelTest.java
@@ -17,60 +17,54 @@ import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 public class HistoryTableModelTest {
 
 	
-	HistoryTableModel htm1;
-	HistoryTableModel htm2;
-	HistoryTableModel htm3;
+	HistoryTableModel htm;
 	HistoryItem historyItem;
 	StockItem stockItem1;
 	StockItem stockItem2;
 	SoldItem soldItem1;
 	SoldItem soldItem2;
+	List<SoldItem> goods;
 	
 	@Before
 	public void setUp() {
-		htm1= new HistoryTableModel();
-		htm2= new HistoryTableModel();
-		htm3= new HistoryTableModel();
 		stockItem1 = new StockItem((long) 1, "Test Item", "For testing", 10.0, 1);
 		stockItem2 = new StockItem((long) 2, "Test Item", "For testing", 11.0, 1);
 		soldItem1 = new SoldItem(stockItem1, 2);
 		soldItem2 = new SoldItem(stockItem2, 1);
-	}
-	
-	@Test 
-	public void testAddSoldItem(){
-		List<SoldItem> goods = new ArrayList<SoldItem>();
-		goods.add(soldItem1);
-		goods.add(soldItem2);
-		historyItem = new HistoryItem("23/11/2014", "01:01:01",
-				new BigDecimal(2 * 11.0+10.0), goods);
-		htm1.addItem(historyItem);
-		int rowCount=htm1.getRowCount();
-		assertEquals(htm1.getTableRows().get(rowCount-1), historyItem);
-	}
-
-	
-	@Test 
-	public void testGetColumnValue(){
-		List<SoldItem> goods = new ArrayList<SoldItem>();
+		goods = new ArrayList<SoldItem>();
 		goods.add(soldItem1);
 		goods.add(soldItem2);
 		historyItem = new HistoryItem("23/11/2014", "01:01:01",
 				new BigDecimal(2 * 11.0+10.0), goods);
 		historyItem.setId((long)1);
-		htm2.addItem(historyItem);
-		assertEquals(htm2.getColumnValue(historyItem, 0), (long)1); //ID
-		assertEquals(htm2.getColumnValue(historyItem, 1), "23/11/2014"); //Date
-		assertEquals(htm2.getColumnValue(historyItem, 2), "01:01:01"); //Time
-		assertEquals(htm2.getColumnValue(historyItem, 3), new BigDecimal(2 * 11.0+10.0).setScale(2, RoundingMode.HALF_UP)); //Cost
+	}
+	
+	@Test 
+	public void testAddItem(){
+		htm= new HistoryTableModel();
+		htm.addItem(historyItem);
+		int rowCount=htm.getRowCount();
+		assertEquals(htm.getTableRows().get(rowCount-1), historyItem);
+	}
+
+	
+	@Test 
+	public void testGetColumnValue(){
+		htm= new HistoryTableModel();
+		htm.addItem(historyItem);
+		assertEquals(htm.getColumnValue(historyItem, 0), (long)1); //ID
+		assertEquals(htm.getColumnValue(historyItem, 1), "23/11/2014"); //Date
+		assertEquals(htm.getColumnValue(historyItem, 2), "01:01:01"); //Time
+		assertEquals(htm.getColumnValue(historyItem, 3), new BigDecimal(2 * 11.0+10.0).setScale(2, RoundingMode.HALF_UP)); //Cost
 	}
 	
 	@Test 
 	public void testClear(){
-		htm3.addItem(historyItem);
-		assertEquals(htm3.getRowCount(), 1);
-		htm3.clear();
-		assertEquals(htm3.getRowCount(), 0);
+		htm= new HistoryTableModel();
+		htm.addItem(historyItem);
+		assertEquals(htm.getRowCount(), 1);
+		htm.clear();
+		assertEquals(htm.getRowCount(), 0);
 	}
 	
 	
